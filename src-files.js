27788effff6f29dc8f951c310d6d39b52d840606
aarkegz <aarkegz@gmail.com>
2025-06-18 07:34:12 +0000

    Deploying to gh-pages from @ arceos-hypervisor/x86_vlapic@7fdd40cd67711cdf8cfa1673f1d62d326d66b1d0 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["x86_vlapic",["",[["regs",[["lvt",[],["cmci.rs","error.rs","lint0.rs","lint1.rs","mod.rs","perfmon.rs","thermal.rs","timer.rs"]],["timer",[],["dcr.rs","mod.rs"]]],["apic_base.rs","dfr.rs","esr.rs","icr.rs","mod.rs","svr.rs"]]],["consts.rs","lib.rs","timer.rs","utils.rs","vlapic.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[286]}
