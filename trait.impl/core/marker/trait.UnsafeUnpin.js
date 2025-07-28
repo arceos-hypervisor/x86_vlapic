e629851efd6573015cd714f9ca2567567cd7f7f2
YanLien <128586861+YanLien@users.noreply.github.com>
2025-07-28 00:45:43 +0000

    Deploying to gh-pages from @ arceos-hypervisor/x86_vlapic@1bcf9feace828cdee555620a1c8ca5408129f1eb 🚀

---
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["x86_vlapic",[["impl UnsafeUnpin for <a class=\"struct\" href=\"x86_vlapic/struct.EmulatedLocalApic.html\" title=\"struct x86_vlapic::EmulatedLocalApic\">EmulatedLocalApic</a>",1,["x86_vlapic::EmulatedLocalApic"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[216]}
